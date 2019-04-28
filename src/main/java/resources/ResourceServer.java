38ad57c039bfeeb83be7b5cdf18fec2c9ac1c93e
Anykey Space <20nomad12@mail.ru>
2019-04-28 22:41:07 +0300

    home work 2-2

---
+++ src/main/java/resources/ResourceServer.java
@@ -0,0 +1,17 @@
+package resources;
+
+public class ResourceServer {
+
+    private TestResource resource;
+
+    public ResourceServer() {
+    }
+
+    public TestResource getResource() {
+        return resource;
+    }
+
+    public void setResource(TestResource resource) {
+        this.resource = resource;
+    }
+}
